4922a5d09dede79ff77ea8c5216cb42b041144a1
nkl708 <nklnsk708@gmail.com>
2023-12-29 04:14:43 +0700

    Add bootstrap 5
    Add 2 basic standalone components (for test)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  standalone: true,
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  imports: [HeaderComponent, FooterComponent],
 })
 export class AppComponent {
-  title = 'frontend';
+  title = "frontend";
 }
